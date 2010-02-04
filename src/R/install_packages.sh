4ab72b007608e07e8530d513bc6cdb717b5e4895
Jim Hayes <jhayes@sdsc.edu>
2010-02-04 21:41:18 +0000

    Switch from /etc/profile.d to modules.

--- src/R/install_packages.sh
+++ src/R/install_packages.sh
@@ -6,26 +6,9 @@
 # during installation, rather than once during the roll build.
 
 PKGROOT=$1
-yum -y install curl-devel
 
 ${PKGROOT}/bin/R --vanilla << END
 repos <- getOption("repos")
 repos["CRAN"] = "http://cran.stat.ucla.edu"
 options(repos = repos)
-install.packages('gam', dependencies = TRUE)
-install.packages('gap', dependencies = TRUE)
-install.packages('haplo.stats', dependencies = TRUE)
-source("http://bioconductor.org/biocLite.R")
-biocLite("KEGG.db")
-biocLite("KEGGSOAP")
-biocLite("biomaRt")
-biocLite("org.Dm.eg.db")
-install.packages('gene2pathway', dependencies = TRUE)
-install.packages('GenABEL', dependencies = TRUE)
-install.packages('gee', dependencies = TRUE)
-install.packages('GWAF', dependencies = TRUE)
-install.packages('kinship', dependencies = TRUE)
-install.packages('lattice', dependencies = TRUE)
-install.packages('nlme', dependencies = TRUE)
-install.packages('survival', dependencies = TRUE)
 END
