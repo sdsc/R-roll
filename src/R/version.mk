bf15bb578891b45a2438b9ba5afe5e6bc7af6c42
Trevor Cooper <tcooper@sdsc.edu>
2013-11-21 12:32:14 -0800

    Updated Makefile and version.mk to use pull.mk

--- src/R/version.mk
+++ src/R/version.mk
@@ -1,5 +1,15 @@
-NAME    = R
-VERSION = 3.0.1
-RELEASE = 0
+NAME               = R
+VERSION            = 3.0.1
+RELEASE            = 0
+PKGROOT            = /opt/R
+RPM.EXTRAS         = "Autoreq: no"
 
-RPM.EXTRAS = "Autoreq: no"
+SRC_SUBDIR         = R
+
+SOURCE_NAME        = $(NAME)
+SOURCE_VERSION     = $(VERSION)
+SOURCE_SUFFIX      = tar.gz
+SOURCE_PKG         = $(SOURCE_NAME)-$(SOURCE_VERSION).$(SOURCE_SUFFIX)
+SOURCE_DIR         = $(SOURCE_PKG:%.$(SOURCE_SUFFIX)=%)
+
+TAR_GZ_PKGS        = $(SOURCE_PKG)
