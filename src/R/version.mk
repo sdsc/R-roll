21294fb35c7b475263f8399b81324cb0a122d694
Jim Hayes <jhayes@sdsc.edu>
2014-08-12 23:01:13 -0700

    Increment RELEASE.

--- src/R/version.mk
+++ src/R/version.mk
@@ -5,7 +5,7 @@ COMPILERNAME := $(firstword $(subst /, ,$(ROLLCOMPILER)))
 
 NAME               = R_$(COMPILERNAME)
 VERSION            = 3.0.1
-RELEASE            = 1
+RELEASE            = 2
 RPM.EXTRAS         = AutoReq:No
 
 SRC_SUBDIR         = R
